64f3e442a375745b8a4f55b8fdfb0a9682763944
maayyy <zfuak@connect.ust.hk>
2025-01-10 11:19:56 +0100

    calcualte the standard error

--- Code/R/Estimation.R
+++ Code/R/Estimation.R
@@ -1,31 +1,29 @@
 rm(list = ls())
-pacman::p_load(data.table, fixest, gmm)
-
-var_exo <- c("hp2wt", "air", "dpm", "size")
-var_end <- c("p")
-var_rand <- c("const") # the random coefficient can be on the const term as well.
-
-# the main part
-
+pacman::p_load(data.table, fixest)
 source("Code/R/Functions.R")
+
+# ---- define the global variables ----
+var_exo <- c("hp2wt", "air", "dpm", "size") # exogenous variables
+var_end <- c("p") # endogenous variables
+var_x <- c(var_exo, var_end) # all the rhs variables
+var_iv_instr <- c("hp2wt_instr", "air_instr", "dpm_instr", "size_instr") # instruments
+var_rand <- c("const") # the variables with random coefficients
+
+
+# ---- the main estimation steps ----
+
 # load the data
 dt <- readRDS("Data/Out/carpanel_q4.rds")
-# draw 500 normal random draws to approximate the integral
+# draw n=500 normal random draws to approximate the integral
 n <- 500
-# draw in the normal(0, 1)
 draw <- rnorm(n)
-# check the variables
-var_x <- c(var_exo, var_end)
-# var_iv_blp <- c("const_rival", "const_ownothers", "dpm_rival", "dpm_ownothers", "hp2wt_rival", "hp2wt_ownothers", "size_rival", "size_ownothers", "air_rival", "air_ownothers")
-# var_iv_nl <- c("const_rival", "const_ownothers", "dpm_rival", "dpm_ownothers", "hp2wt_rival", "hp2wt_ownothers", "air_rival", "air_ownothers", "const_rival_g", "hp2wt_rival_g", "dpm_rival_g", "air_rival_g")
-# var_iv_local <- c("size_local", "wb_local", "hp_local", "hp2wt_local", "disp_local")
-# var_iv_quad <- c("wb_quad", "hp_quad", "size_quad", "hp2wt_local", "disp_quad")
-var_iv_instr <- c("hp2wt_instr", "air_instr", "dpm_instr", "size_instr")
 
 # optim
-setFixest_notes(FALSE) # suppress fixest notes
+setFixest_notes(FALSE) # suppress fixest notes (otherwise, too many notes will be printed in the console)
+# esimate the non-linear parameters sigma and rho
 sigma_opt <- optim(c(2, 0.5), blp_moment_condition, data = dt, var_iv_new = var_iv_instr, var_rand_coef = var_rand)
-sigma <- sigma_opt$par
+sigma <- sigma_opt$par # the non-linear parameters
+# get the linear parameters
 blp_result <- blp_intermediate(sigma, dt, var_iv_instr, var_rand)
 ivreg <- blp_result[[2]]
 summary(ivreg, cluster = "modelid")
@@ -33,28 +31,34 @@ etable(ivreg,
     cluster = "modelid", fitstat = ~ n + ar2 + ivf1 + ivf1.p + sargan + sargan.p,
     file = "Results/param_linear.tex", tex = TRUE, replace = TRUE
 )
-beta <- ivreg$coefficients
+beta <- ivreg$coefficients # the linear parameters
 delta <- blp_result[[1]]
 mu <- blp_result[[3]]
 save(dt, delta, sigma, ivreg, beta, mu, var_exo, var_end, var_rand, file = "Data/Out/blp_results.rda")
-
-# end
-
 
 # joint estimation to get the standard errors with starting values from the previous estimation
 beta <- as.numeric(beta)
 theta_init <- c(sigma, beta)
 
-# theta <- theta_init
-# data <- dt
-# var_iv_new <- var_iv_instr
-# var_rand_coef <- var_rand
-# theta[3:length(theta)]
+theta_opt <- optim(theta_init, blp_moment_condition_full, data = dt, var_iv_new = var_iv_instr, var_rand_coef = var_rand, hessian = TRUE)
+theta <- theta_opt$par # all the parameters
+theta
 
-theta_opt <- optim(theta_init, blp_moment_condition_full, data = dt, var_iv_new = var_iv_instr, var_rand_coef = var_rand)
-theta <- theta_opt$par
+# ---- calculate the standard errors ----
 
-# to get the standard errors
-# the following doesn't work:)
-# hessian <- theta_opt$hessian
-# se <- sqrt(diag(solve(hessian)))
+# need G W Z residual
+pacman::p_load(numDeriv)
+
+f <- function(x) {
+    result <- blp_intermediate_full(theta = x, data = dt, var_iv_new = var_iv_instr, var_rand_coef = var_rand)[[2]]
+    return(result)
+}
+G <- jacobian(f, theta_init)
+
+blp_results_full <- blp_intermediate_full(theta_init, dt, var_iv_instr, var_rand)
+g <- blp_results_full$g
+Z <- blp_results_full$Z
+W <- blp_results_full$W
+residual <- blp_results_full$residual
+
+blp_se <- gmm_se_hetero(G, Z = Z, W = W, residual = residual)
--- Code/R/Functions.R
+++ Code/R/Functions.R
@@ -1,16 +1,20 @@
-# the main part
+# In this file, I define the functions used in the estimation
+
 # given delta_j and simulated draws, calculate the market share
 share <- function(delta, mu) {
-    n <- ncol(mu)
-    numer <- exp(delta + mu)
-    denom <- 1 + colSums(numer) # for each column sum up the rows
-    sij <- numer / denom
-    sj <- rowSums(sij) / n # for each row sum up the columns
+    # J is the number of products, n is the number of draws
+    # delta is of J x 1, mu is of J x n
+    n <- ncol(mu) # get the number of draws
+    numer <- exp(delta + mu) # for each product and each draw, calculate the numerator, dim(numer) = J x n
+    denom <- 1 + colSums(numer) # for each draw, sum up the products, dim(denom) = 1 x n
+    sij <- numer / denom # dim(sij) = J x n
+    sj <- rowSums(sij) / n # for each product, sum up the draws and divide by n, dim(sj) = J x 1
     return(sj)
 }
 
-# given the market share sj, back out delta_j, and the number of iterations
+# given the observed market share sj, back out delta_j, and the number of iterations
 blp_contraction <- function(sj, mu, delta_init) {
+    # dim(sj) = J x 1, dim(mu) = J x n, dim(delta_init) = J x 1
     J <- length(delta_init) # get the number of products
     delta_before <- rep(0, J)
     delta_next <- delta_init
@@ -18,24 +22,30 @@ blp_contraction <- function(sj, mu, delta_init) {
     while (max(abs(delta_next - delta_before)) > 1e-7 && iter < 500) {
         delta_before <- delta_next
         iter <- iter + 1
-        delta_next <- delta_before + log(sj) - log(share(delta_before, mu))
+        delta_next <- delta_before + log(sj) - log(share(delta_before, mu)) # update delta
     }
     delta <- delta_next
     flag_cv <- (iter == 500 || max(is.na(delta)) == 1)
     return(list(delta, flag_cv, iter))
 }
 
-# gmm estimation (concentrate out beta)
+# gmm objective function (estimate the linear parameters to concentrate it out in the objective function)
 blp_moment_condition <- function(theta, data, var_iv_new, var_rand_coef) {
+    # theta is the parameter vector, dim(theta) = n(var_rand_coef) + rho + n(linear parameters)
+    # data is the panel data, ~ name + year
+    # var_iv_new is the instruments used in the 1) IV regression (for estimation of linear parameters) 2) gmm objective function (for estimation of sigma and rho)
+    # var_rand_coef is the variable with random coefficient
     delta_new <- c()
     sigma <- theta[1]
     rho <- theta[2]
     # flag_cv_new <- c()
     # iter_new <- c()
+
+    # step 1: construct the delta_j for each product
     for (i in unique(data$year)) {
         J <- data[year == i, .N]
         sj <- data[year == i, mktshr]
-        mu <- sigma * outer(as.vector(as.matrix(data[year == i, ..var_rand_coef])), draw)
+        mu <- sigma * outer(as.vector(as.matrix(data[year == i, ..var_rand_coef])), draw) # construct the mu matrix of J x n
         delta_init <- data[year == i, log(mktshr) - log(shr_0)]
         contraction_result <- blp_contraction(sj, mu, delta_init)
         delta_market <- contraction_result[[1]]
@@ -43,9 +53,10 @@ blp_moment_condition <- function(theta, data, var_iv_new, var_rand_coef) {
         # flag_cv_new <- c(flag_cv_new, contraction_result[[2]])
         # iter_new <- c(iter_new, contraction_result[[3]])
     }
+
+    # step 2: construct the lagged difference
     # here we need to introduce dynamics
     # for each y construct y_t-\rho*y_{t-1}
-
     data$y <- delta_new
     data <- panel(data, ~ name + year)
     data[, y_ld := y - l(y, 1) * rho]
@@ -53,26 +64,31 @@ blp_moment_condition <- function(theta, data, var_iv_new, var_rand_coef) {
         data[, (paste0(i, "_ld")) := get(i) - l(get(i), 1) * rho]
     }
 
-    # esitmation of linear parameters
+    # step 3: esitmation of linear parameters
     iv_formula <- as.formula(paste("y_ld ~", paste(paste0(var_exo, split = "_ld"), collapse = " + "), "|", paste(paste0(var_end, split = "_ld"), collapse = " + "), "~", paste(var_iv_new, collapse = " + ")))
     iv_reg <- feols(iv_formula, data = data)
     residual <- iv_reg$residuals
     data <- unpanel(data) # unpanel the data
     data_used <- data[obs(iv_reg)]
+
+    # step 4: construct the gmm objective function
     var_xz <- c(paste0(var_exo, split = "_ld"), var_iv_new)
-    Z <- as.matrix(data_used[, ..var_xz], verbose = FALSE)
+    Z <- as.matrix(data_used[, ..var_xz])
     W <- solve(t(Z) %*% Z)
     g <- t(Z) %*% residual
     gmm_obj <- t(g) %*% W %*% g
     return(gmm_obj)
 }
 
+# gmm objective function (estimate all parameters simultaneously without concentrating out the linear parameters first)
 blp_moment_condition_full <- function(theta, data, var_iv_new, var_rand_coef) {
     delta_new <- c()
     sigma <- theta[1]
     rho <- theta[2]
     # flag_cv_new <- c()
     # iter_new <- c()
+
+    # step 1: construct the delta_j for each product
     for (i in unique(data$year)) {
         J <- data[year == i, .N]
         sj <- data[year == i, mktshr]
@@ -84,6 +100,8 @@ blp_moment_condition_full <- function(theta, data, var_iv_new, var_rand_coef) {
         # flag_cv_new <- c(flag_cv_new, contraction_result[[2]])
         # iter_new <- c(iter_new, contraction_result[[3]])
     }
+
+    # step 2: construct the lagged difference
     # here we need to introduce dynamics
     # for each y construct y_t-\rho*y_{t-1}
 
@@ -95,7 +113,7 @@ blp_moment_condition_full <- function(theta, data, var_iv_new, var_rand_coef) {
     }
     data <- unpanel(data)
 
-    # esitmation of linear parameters
+    # step 3: construct the gmm objective function
     y_lhs <- as.vector(data[, y_ld])
     obs_used <- !is.na(y_lhs)
     y_lhs <- y_lhs[obs_used]
@@ -104,21 +122,22 @@ blp_moment_condition_full <- function(theta, data, var_iv_new, var_rand_coef) {
     x_rhs <- as.matrix(data[, ..rhs])
     x_rhs <- x_rhs[obs_used, ]
 
-    residual <- y_lhs - x_rhs %*% theta[3:length(theta)]
+    residual <- y_lhs - x_rhs %*% theta[3:length(theta)] # residual
     var_xz <- c(paste0(var_exo, split = "_ld"), var_iv_new)
+    Z <- as.matrix(data[obs_used, ..var_xz]) # instruments
 
-    Z <- as.matrix(data[obs_used, ..var_xz])
     W <- solve(t(Z) %*% Z)
     g <- t(Z) %*% residual
     gmm_obj <- t(g) %*% W %*% g
     return(gmm_obj)
 }
 
+# for the blp_moment_condition function, the linear parameters are estimated before constructing the gmm function
+# blp_intermediate is to return the linear parameters estimated within the blp_moment_condition function
 blp_intermediate <- function(theta, data, var_iv_new, var_rand_coef) {
     delta_new <- c()
     sigma <- theta[1]
     rho <- theta[2]
-    var_rand <- var_rand_coef
     mu_new <- c()
     # flag_cv_new <- c()
     # iter_new <- c()
@@ -143,4 +162,66 @@ blp_intermediate <- function(theta, data, var_iv_new, var_rand_coef) {
     iv_formula <- as.formula(paste("y_ld ~", paste(paste0(var_exo, split = "_ld"), collapse = " + "), "|", paste(paste0(var_end, split = "_ld"), collapse = " + "), "~", paste(var_iv_new, collapse = " + ")))
     reg_iv <- feols(iv_formula, data = data, cluster = "modelid")
     return(list(delta_new, reg_iv, mu_new))
+}
+
+# return the delta, instruments, weights, and residuals from the gmm joint estimation
+blp_intermediate_full <- function(theta, data, var_iv_new, var_rand_coef) {
+    delta_new <- c()
+    sigma <- theta[1]
+    rho <- theta[2]
+    # flag_cv_new <- c()
+    # iter_new <- c()
+
+    # step 1: construct the delta_j for each product
+    for (i in unique(data$year)) {
+        J <- data[year == i, .N]
+        sj <- data[year == i, mktshr]
+        mu <- sigma * outer(as.vector(as.matrix(data[year == i, ..var_rand_coef])), draw)
+        delta_init <- data[year == i, log(mktshr) - log(shr_0)]
+        contraction_result <- blp_contraction(sj, mu, delta_init)
+        delta_market <- contraction_result[[1]]
+        delta_new <- c(delta_new, delta_market)
+        # flag_cv_new <- c(flag_cv_new, contraction_result[[2]])
+        # iter_new <- c(iter_new, contraction_result[[3]])
+    }
+
+    # step 2: construct the lagged difference
+    # here we need to introduce dynamics
+    # for each y construct y_t-\rho*y_{t-1}
+
+    data$y <- delta_new
+    data <- panel(data, ~ name + year)
+    data[, y_ld := y - l(y, 1) * rho]
+    for (i in c("const", var_end, var_exo)) {
+        data[, (paste0(i, "_ld")) := get(i) - l(get(i), 1) * rho]
+    }
+    data <- unpanel(data)
+
+    # step 3: construct the gmm objective function
+    y_lhs <- as.vector(data[, y_ld])
+    obs_used <- !is.na(y_lhs)
+    y_lhs <- y_lhs[obs_used]
+
+    rhs <- paste0(c("const", var_end, var_exo), split = "_ld")
+    x_rhs <- as.matrix(data[, ..rhs])
+    x_rhs <- x_rhs[obs_used, ]
+
+    residual <- y_lhs - x_rhs %*% theta[3:length(theta)] # residual
+    var_xz <- c(paste0(var_exo, split = "_ld"), var_iv_new)
+    Z <- as.matrix(data[obs_used, ..var_xz]) # instruments
+
+    W <- solve(t(Z) %*% Z)
+    g <- t(Z) %*% residual
+    gmm_obj <- t(g) %*% W %*% g
+    return(list(delta = delta_new, g = g, Z = Z, W = W, residual = residual))
+}
+
+# calucate the standard errors following the heteroskedasticity robust formula (without efficient weighting matrix)
+# G_tWG_inv %*% (t(G) %*% W %*% Omega %*% W %*% G) %*% G_tWG_inv
+gmm_se_hetero <- function(G, Z, W, residual) {
+    Omega <- t(Z) %*% diag(as.vector(residual^2)) %*% Z / nrow(Z)
+    G_tWG_inv <- solve(t(G) %*% W %*% G)
+    robust_var_cov_matrix <- G_tWG_inv %*% (t(G) %*% W %*% Omega %*% W %*% G) %*% G_tWG_inv
+    se <- sqrt(diag(robust_var_cov_matrix))
+    return(list(se, robust_var_cov_matrix))
 }
